caf031e426fd6e9527a1199586836b7c687c3892
raheescv <raheescv1992@gmail.com>
2024-10-11 08:16:06 +0530

    front - end initial update

---
+++ frontend/pages/products/[id].vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h2>product details for {{ id }}</h2>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis, soluta.</p>
+  </div>
+</template>
+
+<script setup>
+definePageMeta({
+  layout: 'products'
+});
+const { id } = useRoute().params;
+
+</script>
+
+<style scoped></style>
---
+++ frontend/pages/products/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h2>Product</h2>
+    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Veniam molestias eos odio odit distinctio voluptas enim
+      deserunt quia, quas repellat.</p>
+  </div>
+</template>
+
+<script setup>
+definePageMeta({
+  layout: 'products'
+});
+</script>
+
+<style></style>
